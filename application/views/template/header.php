44c30988b10a1c267eee328d6104deeaae57357a
Ednei Leite da Silva <edsiddos@gmail.com>
2016-01-26 16:30:33 -0300

    Atualização arquivos estáticos

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -36,7 +36,7 @@
 
         <!-- Fixed navbar -->
         <nav class="navbar navbar-default navbar-fixed-top">
-            <div class="container">
+            <div class="container-fluid">
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                         <span class="sr-only">phpMyCall</span>
@@ -126,4 +126,4 @@
         </nav>
 
         <!-- Begin page content -->
-        <div class="container">
+        <div class="container-fluid">
